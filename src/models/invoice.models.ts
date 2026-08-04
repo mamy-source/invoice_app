00e15bffa6929d10194aa5e3c0284f2edfd0554b
Mamy <mamitianarandrianantenaina253@gmail.com>
2026-08-04 09:37:21 +0300

    models

---
+++ src/models/invoice.models.ts
@@ -0,0 +1,53 @@
+export interface Product {
+    name: string;
+    quantity: number;
+    unitPrice: number;
+}
+
+export interface CreateInvoice {
+    clientEmail: string;
+    products: Product[];
+}
+
+export interface InvoiceResponse {
+    id: string;
+    invoiceNumber: string;
+    clientEmail: string;
+    products: ProductResponse[];
+    total: number;
+    createdAt: Date;
+    emailSent: boolean;
+    emailError?: string | null;
+}
+
+export interface ProductResponse {
+    id: string;
+    name: string;
+    quantity: number;
+    unitPrice: number;
+}
+
+//Domain models
+export interface InvoiceWithProducts {
+    id: string;
+    invoiceNumber: string;
+    clientEmail: string;
+    total: number;
+    createdAt: Date;
+    updatedAt: Date;
+    pdfPath: string | null;
+    emailSent: boolean;
+    emailError: string | null;
+    products: ProductWithInvoice[];
+}
+
+export interface ProductWithInvoice {
+    id: string;
+    invoiceId: string;
+    name: string;
+    quantity: number;
+    unitPrice: number;
+    createdAt: Date;
+    updatedAt: Date;
+  }
+
